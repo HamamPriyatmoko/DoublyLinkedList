6ac51b4634ec28c56a532921dbd3f22d88c7cbf1
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2022-11-17 16:30:21 +0700

    membuat fungtion node

--- DoublyLinkedList/Program.cs
+++ DoublyLinkedList/Program.cs
@@ -6,6 +6,17 @@ using System.Threading.Tasks;
 
 namespace DoublyLinkedList
 {
+    class Node
+    {
+        /*Node class represents the node of doubly linked list.
+         * it consists of the information part and links to
+         * its succeeding and preceeding nodes
+         * in terms of next and previous nodes.*/
+        public int rollNumber;
+        public string name;
+        public Node next;/*points to the succeeding node*/
+        public Node prev;/*points to the preceeding node*/
+    }
     internal class Program
     {
         static void Main(string[] args)
